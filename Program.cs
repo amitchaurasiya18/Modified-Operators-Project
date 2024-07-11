de51c04f63b9e3d0bfa1d2149a5f61d250758ade
Amit Dharmandra Chaurasiya <Amit.Chaurasiya@asl.mum.sst>
2024-07-11 14:44:28 +0530

    Initial Copy Paste

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedOperatorsProject
+{
+    internal class Program
+    {
+        public const int DIVISOR = 2;
+        static void Main(string[] args)
+        {
+            //int number1 = 10, number2 = 20, number3 = 0;
+
+            //number3 = number1+++number1;
+            //Console.WriteLine(number3);
+            //Console.WriteLine(number1);
+
+            //number3 = number1++ + --number2;
+            //Console.WriteLine(number3);
+            //Console.WriteLine(number1);
+            //Console.WriteLine(number2);
+
+            //number3 = number1++ + ++number1;
+            //Console.WriteLine(number3);
+            //Console.WriteLine(number1);
+
+            Console.Write("Enter a number to check if it is a perfect number: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(CheckPerfectNumber(inputNumber));
+        }
+        
+        static string CheckPerfectNumber(int number)
+        {
+            int iterator = 1;
+            int sum = 0;
+            while (iterator <= number / 2)
+            {
+                if (number % iterator == 0)
+                    sum += iterator;
+                iterator++;
+            }
+
+            return sum == number ? "It is a Perect Number." : "It is not a Perfect Number.";
+        }
+    }
+}
